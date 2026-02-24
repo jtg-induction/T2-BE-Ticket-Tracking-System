29c5e91a2f9261b3583219b4ede03652c5d03ae0
Anmol <anmol@joshtechnologygroup.com>
2026-02-24 17:21:02 +0530

    [A-0]:Base Setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,19 +10,24 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
-INSTALLED_APPS = [
-    #Django apps
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    #Third party apps
-    'rest_framework',
-    'rest_framework_simplejwt'
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
+
+LOCAL_APPS = [
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
